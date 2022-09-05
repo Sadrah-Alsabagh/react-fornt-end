6d6f4c4b4ef0bcad96f0b1ed3c032928c9f20418
Sadrah Alsabagh <106437503+Sadrah-Alsabagh@users.noreply.github.com>
2022-09-05 21:27:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <h1 className="header" data-testid="appName"> Identity Form App </h1>
       <Person />
-      <p className="footer">Identity Form App &copy;All Rights Reserved | Saeed Kokash </p>  
     </div>
   );
 }
