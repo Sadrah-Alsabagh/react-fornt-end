348791a224464dfb1913e53b3e143a43084bb636
Sadrah-alsabagh <sedrasabbagh33@gmail.com>
2022-09-06 23:44:40 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="header" data-testid="appName">Form </h1>
+      <h1 className="header" data-testid="appName"> Form </h1>
       <Person />
     </div>
   );
